dccff4d018943f6b11fe2d66ca765f6e89fbbfe1
g2384 <g2384@users.noreply.github.com>
2023-04-01 15:41:12 +0100

    fix tests

--- decipher.test.js
+++ decipher.test.js
@@ -1,41 +1,34 @@
 import { decipher, replaceAll } from './decipher';
-
-var collection: any = [];
-
-const printCallback = jest.fn((input: any[], text: string) => {
+var collection = [];
+const printCallback = jest.fn((input, text) => {
     collection.push(input.concat([text]));
 });
-
-function getTestName(expect: jest.Expect): string {
-    return (expect.getState().currentTestName?.toString() ?? "").replace("Decipher pinyin ", "");
+function getTestName(expect) {
+    var _a, _b;
+    return ((_b = (_a = expect.getState().currentTestName) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : "").replace("Decipher pinyin ", "");
 }
-
 describe("Decipher pinyin", () => {
     test('aa', () => {
         collection = [];
-        let name: string = getTestName(expect);
+        let name = getTestName(expect);
         decipher(name, '', '', '', 0, printCallback);
         expect(collection.length).toBe(1);
-        expect(collection[0]).toEqual([true, 'a a ', 5])
+        expect(collection[0]).toEqual([true, 'a a ', 5]);
     });
-
     test('aab', () => {
         collection = [];
-        let name: string = getTestName(expect);
+        let name = getTestName(expect);
         decipher(name, '', '', '', 0, printCallback);
         expect(collection.length).toBe(0);
     });
-
     test('aabb', () => {
         collection = [];
-        let name: string = getTestName(expect);
+        let name = getTestName(expect);
         decipher(name, '', '', '', 0, printCallback);
         expect(collection.length).toBe(1);
-        expect(collection[0]).toEqual([true, 'a a ', 5])
+        expect(collection[0]).toEqual([true, 'a a ', 5]);
     });
 });
-
-
 describe("Replace all", () => {
     test('replace letter', () => {
         expect(replaceAll('abc', 'a', 'b')).toBe("bbc");

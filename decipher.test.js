b33da3237bf3adb04dc5b951f98f792e39171415
g2384 <g2384@users.noreply.github.com>
2023-04-01 17:17:26 +0100

    fix unit tests

--- decipher.test.js
+++ decipher.test.js
@@ -7,7 +7,7 @@ const printCallback = jest.fn((input, text) => {
 });
 function getTestName(expect) {
     var _a, _b;
-    return ((_b = (_a = expect.getState().currentTestName) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : "").replace("Decipher pinyin ", "");
+    return ((_b = (_a = expect.getState().currentTestName) === null || _a === void 0 ? void 0 : _a.toString()) !== null && _b !== void 0 ? _b : "").replace("Decipher pinyin ", "").toUpperCase();
 }
 describe("Decipher pinyin", () => {
     test('aa', () => {
